b32661cae8d6debbf24ce8693fede156d42c7324
CindyAbi <111438069+CindyAbi@users.noreply.github.com>
2026-07-31 12:53:37 +0930

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackStart(), // CRITICAL: This plugin must run first to handle your routes and start.ts file
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
