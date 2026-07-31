a0cbcf60847a46708421e684440209fa4ec83c0b
CindyAbi <111438069+CindyAbi@users.noreply.github.com>
2026-07-31 13:51:09 +0930

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-mport { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
